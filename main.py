dccd3a35734748cc55674dd01b6ae87d5d95a2c9
thhung <nguyen.thanh.hung.91@gmail.com>
2019-09-04 13:25:47 +0200

    create the skeleton for main

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import gc
+import os.path as osp
+import warnings
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Sequential, Linear, ReLU, GRU
+import torch.optim as optim
+
+from torch_geometric.data import Data, DataLoader
+from torch_geometric.data import InMemoryDataset
+from torch_geometric.nn import NNConv, Set2Set, AGNNConv
+import torch_geometric.transforms as T
+from torch_geometric.data import DataLoader
+from torch_geometric.utils import remove_self_loops
+# from torch_geometric.datasets import QM9
+# from torch_geometric.nn import MessagePassing
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+
+from tqdm import tqdm
+
+
+warnings.filterwarnings('ignore')
+
+
+def main():
+  train_val_test_df = pd.read_parquet('train_test.parquet', engine='fastparquet', 
+                                columns = ['atom_index_0', 'atom_index_1', 'molecule_name', 
+                                           'scalar_coupling_constant','type','dist','inv_dist',
+                                           'inv_dist_p_2','inv_dist_p_3', 'coulomb_0_1'])
+  pass
+  
+if __name__=="__main__":
+    main()
+
