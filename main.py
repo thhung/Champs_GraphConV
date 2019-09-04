4fadd6b600790b790cfada00db3b32d65ef8358d
thhung <nguyen.thanh.hung.91@gmail.com>
2019-09-04 13:38:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 from torch.nn import Sequential, Linear, ReLU, GRU
 import torch.optim as optim
 
 from torch_geometric.data import Data, DataLoader
 from torch_geometric.data import InMemoryDataset
 from torch_geometric.nn import NNConv, Set2Set, AGNNConv
+from torch_geometric.nn import GCNConv, SGConv, ChebConv, DynamicEdgeConv, XConv
 import torch_geometric.transforms as T
 from torch_geometric.data import DataLoader
 from torch_geometric.utils import remove_self_loops
@@ -25,6 +27,7 @@ from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 
 from tqdm import tqdm
+from dataset import ChampsDataset
 
 
 warnings.filterwarnings('ignore')
@@ -39,6 +42,26 @@ def main():
                                            'scalar_coupling_constant','type','dist','inv_dist',
                                            'inv_dist_p_2','inv_dist_p_3', 'coulomb_0_1'])
     structures_df = pd.read_csv('structures.csv')
+    train_set = ChampsDataset(train_df, structures_df, './processed_node', './', debug=2560, add_ele=False,
+                      saved_name='train_data.pt', save_id = False, train=True)
+    val_set = ChampsDataset(val_df, structures_df, './processed_node', './', debug=1280, add_ele=False,
+                      saved_name='val_data.pt', save_id = False, train=True)
+    
+    ## if you notice that there is no test set, because the test set is separated here. 
+    ## This is the experiment to improve the Local Validation score. 
+    
+    ## parameter to control the networks
+    dim = 256
+    egde_attr_size = 8
+    input_size = 10 if train_set.add_ele else 8
+
+    OUT_SIZE = 1
+    D_LR = 0.001
+    NB_EPOCH = 300
+    BATCH_SZ = 64
+    PRINT_EACH = 1000
+    
+    
     pass
   
   
